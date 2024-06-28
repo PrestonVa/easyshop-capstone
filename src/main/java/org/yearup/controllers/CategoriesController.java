2dd07c552ae153bf24e3e6b977da94a15e003ad2
PrestonVa <vang.preston1@gmail.com>
2024-06-27 22:20:23 -0400

    Finished

---
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -0,0 +1,66 @@
+package org.yearup.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.data.CategoryDao;
+import org.yearup.data.ProductDao;
+import org.yearup.models.Category;
+import org.yearup.models.Product;
+
+import java.util.List;
+@RestController
+@RequestMapping("/categories")
+@CrossOrigin(origins = "http://localhost:8080")
+public class CategoriesController {
+    private final CategoryDao categoryDao;
+    private final ProductDao productDao;
+
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
+    }
+    @GetMapping
+    public List<Category> getAllCategories() {
+
+        return categoryDao.getAllCategories();
+    }
+
+    @GetMapping("/{id}")
+    public Category getById(@PathVariable int id) {
+        Category category = categoryDao.getById(id);
+        if (category == null )
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return categoryDao.getById(id);
+    }
+
+    @GetMapping("/{categoryId}/products")
+    public List<Product> getProductsById(@PathVariable int categoryId) {
+
+        return productDao.listByCategoryId(categoryId);
+    }
+
+    @PostMapping(path="")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ResponseStatus(value=HttpStatus.CREATED)
+    public Category addCategory(@RequestBody Category category) {
+
+        return categoryDao.create(category);
+    }
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void updateCategory(@PathVariable int id, @RequestBody Category category) {
+        categoryDao.update(id, category);
+
+    }
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void deleteCategory(@PathVariable int id) {
+        categoryDao.delete(id);
+    }
+}
